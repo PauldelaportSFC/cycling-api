422d3223df3adf5ffbbedf99c4da79d5c6e7db4c
Paul de la Port <paul.delaport44@Pauls-iMac.local>
2026-03-17 15:34:46 -0700

    correct intervals auth using API_KEY username

--- server.js
+++ server.js
@@ -14,8 +14,9 @@ const PORT = process.env.PORT || 3000;
 const api = axios.create({
   baseURL: "https://intervals.icu/api/v1",
   auth: {
-    username: process.env.INTERVALS_API_KEY,
-    password: ""
+  	username: process.env.INTERVALS_USERNAME
+	password: process.env.INTERVALS_API_KEY
+
   }
 });
 
